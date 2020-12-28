b7a888c2d8adff5c5c78e09f7c8ca5dfbdf9a56c
aabdullahzafar1 <72663333+aabdullahzafar1@users.noreply.github.com>
2020-12-28 19:36:37 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -42,6 +42,7 @@ import AuthContext from "./app/Auth/context";
 import storage from "./app/Auth/storage";
 import {AppLoading} from 'expo'
 import MapScreen from "./app/screens/MapScreen";
+import { navigationRef } from "./app/navigation/rootNavigation";
 
 LogBox.ignoreLogs(['Setting a timer'])
 
@@ -59,7 +60,22 @@ if (!firebase.apps.length) {
 firebase.initializeApp(firebaseConfig);
 }
 
+const initialHTMLContent = `
 
+<!--player code begin-->
+<div id="svp_playerdlxqec5f9kow" style="width:720px;height:810px;position:relative;">
+	
+</div>
+<script language="javascript" type="text/javascript" src="//play.streamingvideoprovider.com/js/dplayer.js"></script>
+<script language="javascript">
+<!--
+var vars = {clip_id:"dlxqec5f9kow",transparent:"true",pause:"1",repeat:"",bg_color:"#ffffff",fs_mode:"2",no_controls:"",start_img:"0",start_volume:"0",close_button:"",brand_new_window:"1",auto_hide:"1",stretch_video:"",player_align:"NONE",offset_x:"0",offset_y:"0",player_color_ratio:0.6,skinAlpha:"50",colorBase:"#250864",colorIcon:"#ffffff",colorHighlight:"#7f54f8",direct:"false",is_responsive:"true",viewers_limit:0,cc_position:"bottom",cc_positionOffset:70,cc_multiplier:0.03,cc_textColor:"#ffffff",cc_textOutlineColor:"#ffffff",cc_bkgColor:"#000000",cc_bkgAlpha:0.1,aspect_ratio:"16:9",play_button:"1",play_button_style:"pulsing",sleek_player:"1"};
+var svp_player = new SVPDynamicPlayer("svp_playerdlxqec5f9kow", "", "100%", "100%", {use_div:"svp_playerdlxqec5f9kow",skin:"3"}, vars);
+svp_player.execute();
+//-->
+</script>
+<noscript>Your browser does not support JavaScript! JavaScript is needed to display this video player.</noscript>
+<!--player code end-->`;
 
 export default function App() {
 const [user,setUser]=useState()
@@ -68,7 +84,8 @@ const [isReady,setIsReady]=useState(false)
 
 
 firebase.auth().onAuthStateChanged(function(fUser) {
-  if (fUser) {
+  if (fUser.emailVerified===true) {
+    
     setUser(fUser)
   } else {
     setUser(null)
@@ -77,12 +94,14 @@ firebase.auth().onAuthStateChanged(function(fUser) {
 
 const restoreToken = async () => {
   try {
+    const ev = await firebase.auth().currentUser
     const u = await firebase.auth().currentUser
     setUser(u)
     const token = JSON.parse(await storage.getToken())
     if(!token) return
     setUserDetails(token)
     console.log(token)
+    console.log(u)
     
   } catch (error) {
     console.log(error)
@@ -103,9 +122,12 @@ const netInfo = useNetInfo()
    if(!isReady){ 
    return <AppLoading startAsync={restoreToken} onFinish={()=>setIsReady(true)} />
  }
+
+
+
   return  (
     <AuthContext.Provider value={{userDetails, setUserDetails}}>
-    <NavigationContainer theme = {navigationTheme}>
+    <NavigationContainer ref = {navigationRef} theme = {navigationTheme}>
       {userDetails ? <AppNavigator></AppNavigator> : <AuthNavigator></AuthNavigator>}
     </NavigationContainer>
     </AuthContext.Provider>
